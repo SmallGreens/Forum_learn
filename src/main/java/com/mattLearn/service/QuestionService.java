6c4266f4d381187bf75855ff368c0bcc5a1a1ce4
cq <cqyin@outlook.com>
2021-01-08 15:56:33 +0800

    initial commit.

---
+++ src/main/java/com/mattLearn/service/QuestionService.java
@@ -0,0 +1,21 @@
+package com.mattLearn.service;
+
+import com.mattLearn.dao.QuestionDAO;
+import com.mattLearn.model.Question;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * Created by nowcoder on 2016/7/15.
+ */
+@Service
+public class QuestionService {
+    @Autowired
+    QuestionDAO questionDAO;
+
+    public List<Question> getLatestQuestions(int userId, int offset, int limit) {
+        return questionDAO.selectLatestQuestions(userId, offset, limit);
+    }
+}
---
+++ src/main/java/com/mattLearn/service/UserService.java
@@ -0,0 +1,23 @@
+package com.mattLearn.service;
+
+import com.mattLearn.dao.UserDAO;
+import com.mattLearn.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by nowcoder on 2016/7/2.
+ */
+@Service
+public class UserService {
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+    @Autowired
+    private UserDAO userDAO;
+
+    public User getUser(int id) {
+        return userDAO.selectById(id);
+    }
+
+}
